8ee439f3927be67824a3414426816982913685cf
safal-dev <info.safalpoudel@gmail.com>
2026-02-27 15:06:33 +0545

    changing the title

--- admin_ui/frontend/src/components/layout/Sidebar.tsx
+++ admin_ui/frontend/src/components/layout/Sidebar.tsx
@@ -69,7 +69,7 @@ const Sidebar = () => {
                     <div className="w-8 h-8 bg-primary rounded-lg flex items-center justify-center text-primary-foreground">
                         <Activity className="w-5 h-5" />
                     </div>
-                    Asterisk AI
+                    Biz Voice
                 </div>
             </div>
 
